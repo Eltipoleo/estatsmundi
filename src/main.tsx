ba3a631b7bd54d6b8e5f51296c1c0afe1c54c373
Eltipoleo <joserty83@gmail.com>
2026-07-10 02:39:41 -0600

    Fix: limpiar main.tsx e index.html para corregir error de inicializacion de React

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import { AuthProvider } from "./context/AuthContext"
-import "./index.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css' // O el nombre de tu archivo de estilos globales, si no usa CSS bórralo
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
